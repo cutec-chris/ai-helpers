db0a9a304245bc9cbfb66719537f636025cc8401
Christian Ulrich <christian@ullihome.de>
2024-06-12 23:07:39 +0200

    ignore comments and clear lines

--- env.py
+++ env.py
@@ -2,6 +2,8 @@ def load(file_path):
     env = {}
     with open(file_path, 'r') as f:
         for line in f:
-            key, value = line.strip().split('=', 1)
-            env[key] = value
+            if line != '\n'\
+            and not line.startswith('#'):
+                key, value = line.strip().split('=', 1)
+                env[key] = value
     return env
